07c2fc9904dce3d00cb8e08311dc4d0ff4bf304a
Roberto Montoni <robertomontoni.dev@gmail.com>
2024-04-27 07:29:37 -0300

    chore: add card/button

---
+++ app/_components/header.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { MenuIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { Card, CardContent } from "./ui/card";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+
+const Header = () => {
+  return (
+    <header>
+      <Card>
+        <CardContent className="p-5 justify-between items-center flex flex-row">
+          <Link href="/">
+            <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+          </Link>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon">
+                <MenuIcon size={16} />
+              </Button>
+            </SheetTrigger>
+
+            <SheetContent className="p-0">
+            </SheetContent>
+          </Sheet>
+        </CardContent>
+      </Card>
+    </header>
+  );
+};
+
+export default Header;
